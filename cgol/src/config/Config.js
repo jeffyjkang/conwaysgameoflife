f4b621af4b19c9bdb9cf088b57c94066d755b334
jeffyjkang <jeffyjkang@gmail.com>
2019-06-25 18:08:26 -0700

    finish styling

--- cgol/src/config/Config.js
+++ cgol/src/config/Config.js
@@ -4,24 +4,38 @@ import "./Config.css";
 const ConfigContainer = props => {
   return (
     <div className="configContainer">
-      <form>
-        Change speed of game (100ths/sec): {"  "}
-        <input
-          type="number"
-          min={0}
-          max={100}
-          name="speed"
-          onChange={props.setSpeed}
-        />
-      </form>
-      <form onSubmit={props.controlGame}>
-        <button type="submit">{props.running ? "Pause" : "Start"}</button>
-      </form>
-      <form onSubmit={props.resetGame}>
-        <button type="submit">Reset Game</button>
-      </form>
       <div>
-        {"  "}Generation: {props.generation}{" "}
+        <form>
+          <span>Change speed of game (100ths/sec): {"  "} </span>
+          <input
+            className="cInput"
+            type="number"
+            min={0}
+            max={100}
+            placeholder="1-100"
+            name="speed"
+            onChange={props.setSpeed}
+          />
+          <p>(Default 1 sec, *alter while running allowed)</p>
+        </form>
+      </div>
+      <div className="gController">
+        <form onSubmit={props.controlGame}>
+          <button
+            className={props.running ? "paButton" : "stButton"}
+            type="submit"
+          >
+            {props.running ? "Pause" : "Start"}
+          </button>
+        </form>
+        <form onSubmit={props.resetGame}>
+          <button className="reButton" type="submit">
+            Reset Game
+          </button>
+        </form>
+        <div className="cText">
+          {"  "}Generation: {props.generation}{" "}
+        </div>
       </div>
     </div>
   );
